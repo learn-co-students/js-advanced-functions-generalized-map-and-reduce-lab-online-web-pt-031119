05de5ab712ca601e0db23dfb21c174a709d579a8
Ryan <kerneltux0@gmail.com>
2019-10-23 11:49:23 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your functions here
+function map(source,callback){
+  let newArray = [];
+  for(var i=0; i<source.length; i++){
+    newArray.push(callback(source[i]));
+  };
+  return newArray;
+}
+
+function reduce(source,callback,startVal){
+  if(!!startVal){
+    var result = startVal;
+    var i=0;
+  }else{
+    var result = source[0];
+    var i=1;
+  }
+    for(i; i<source.length; i++){
+      result = callback(source[i],result);
+    };
+    return result;
+    
+}
