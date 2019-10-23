faecc9fb580b9bccd2a9aa13f2e0a98782b67a36
vulcanism <cynomania@gmail.com>
2019-10-23 13:08:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your functions here
+function map(sourceArray, fnc) {
+    const transformed = []
+    for (const element of sourceArray) {
+        transformed.push(fnc(element))
+    }
+    return transformed
+}
+
+function reduce(sourceArray, fnc, startingPoint=0) {
+    let total 
+    if (startingPoint) {
+      total = startingPoint   
+    } else {
+        total = sourceArray.shift()
+    }
+    for (const element of sourceArray) {
+      total = fnc(element, total)  
+    }
+    return total 
+}
+
