531a85e2fa069533cdaefa3ac2085848c425c763
Kelly Sciandra <kellysciandradev@icloud.com>
2019-10-25 13:02:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your functions here
+function map(sourceArray, x) {
+    let array = []
+
+    for (let i = 0; i < sourceArray.length; i++) {
+        let whatever = sourceArray[i]
+        array.push(x(whatever))
+    }
+
+    return array;
+}
+
+function reduce(sourceArray, x, starting) {
+    let whatever = (!!starting) ? starting : sourceArray[0]
+    let i = (!!starting) ? 0 : 1
+
+    for (; i < sourceArray.length; i++) {
+        whatever = x(sourceArray[i], whatever)
+    }
+
+    return whatever;
+}
