4b32e65b4dd97e61bd8e51597c8be2c4472ab7b3
dsstewa <davidstewart@gmail.com>
2019-11-01 18:32:15 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+
+const map = (array, func) => {
+   let r = []
+   for (let i = 0; i < array.length; i++) {
+       r.push(func(array[i]))
+   }
+   return r
+}
+
+const reduce = (array, func, start) => {
+    let r = start ? start : array[0]
+    let i = start ? 0 : 1
+    for (; i < array.length; i++) {
+        r = func(array[i], r)
+    }
+    return r
+ }
