287f5dad4943d070f535bd553345737fa8e5e232
Danieleclima <daniel.echuaca.lima@gmail.com>
2019-11-02 19:23:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+function map(array, block){
+    let newArray = []
+    array.forEach(element => {
+        newArray.push(block(element))
+    });
+    return newArray
+}
+
+function reduce(sourceArray, block, startingpoint){
+    let start
+    let i
+if(startingpoint){
+    start = startingpoint
+    i = 0
+} else {
+    start = sourceArray[0]
+    i = 1
+}
+for(; i < sourceArray.length; i++){
+    start = block(sourceArray[i], start)
+}
+return start
+}
