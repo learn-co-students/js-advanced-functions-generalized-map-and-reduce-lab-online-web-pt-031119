8a9ca3d0170da3e88e8eb048ed7a7f2946004b2d
Michael Mottola <mottola64@gmail.com>
2019-11-23 21:09:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Add your functions here
+function map(sourceArray, operation) {
+    let returnedArray = [];
+    sourceArray.forEach(function(element) {
+      returnedArray.push(operation(element));
+    });
+    return returnedArray;
+  }
+  function reduce(array, callback, startingPoint) {
+    let total = startingPoint ? startingPoint : array[0]
+  
+    for (let i = startingPoint ? 0 : 1; i < array.length; i++) {
+      total = callback(array[i], total)
+    }
+    return total
+  }
